07e111bb59a91f818c015261aaffe21a67f0d198
Aditya Rathore <153427299+AdityaDRathore@users.noreply.github.com>
2024-07-25 19:35:41 +0000

    HTTP Json Data done

--- Week 2/Week 2,1/HTTP Server.js
+++ Week 2/Week 2,1/HTTP Server.js
@@ -1,10 +1,13 @@
 import express from 'express'; 
 const express = require('express'); 
-const port = 3000;
+const port = 5000;
 const app = express();
 
 app.get('/', function(req, res) {
-    res.send("Hello World"); 
+    res.json({
+        name: "Aditya",
+        age: 20
+    })
 })
 
 app.listen(port);
