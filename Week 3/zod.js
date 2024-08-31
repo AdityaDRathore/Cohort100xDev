b18d6997d0be334d3bcd3118232249820e5c379e
Aditya Rathore <153427299+AdityaDRathore@users.noreply.github.com>
2024-08-31 18:49:35 +0000

    zod library learning completed

--- Week 3/zod.js
+++ Week 3/zod.js
@@ -2,15 +2,34 @@ const express = require('express');
 const zod = require('zod'); 
 const app = express(); 
 
-const schema = zod.array(zod.number()); 
+/*
+    {
+        email: @__.com
+        password : 8 letters
+        country : IN || US
+    }
+ */
+
+const schema = zod.object({
+    email: zod.string().email(), 
+    password: z.string().min(8), 
+    country: z.literal("IN").or(z.literal('US')),
+    kidneys: z.array(z.number()) 
+})
+
 
 app.use(express.json()); 
 
-app.post('/health-checkup', function(res, req) {
+app.post('/health-checkup', function(req, res) {
     const kidneys = req.body.kidneys; 
     const response = schema.safeParse(kidneys); 
-    const kidneyslenth = kidneys.length; 
-    
+    const kidneyslenth = kidneys.length;
+    if(!response.success){
+        res.status(411).json({
+            message : "Invalid output"
+        })
+    } 
+    else 
     res.send({
         response
     }); 
